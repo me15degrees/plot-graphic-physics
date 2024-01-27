2b0705e9a210e98ec0e25b961cc2eb25a144bb0d
Maria Eduarda <me15degrees@gmail.com>
2024-01-27 16:07:20 -0300

    formatação da saída all_diff e all_int

--- src/all_differentiate.py
+++ src/all_differentiate.py
@@ -1,6 +1,6 @@
 from sympy import symbols, diff
 
-def find_functions(acc_values, time_values, pos_values):
+def find_functions(acc_values, time_intervals, pos_values):
     t, v0, s0 = symbols("t v0 s0")
 
     size = len(acc_values)
@@ -9,7 +9,7 @@ def find_functions(acc_values, time_values, pos_values):
         # Encontrar a função de aceleração e velocidade a partir da posição
         Vd_t, Ad_t = differentiate(pos_values[i], t, v0, s0)
 
-        print(f"\nIntervalo que começa em {time_values[i]}s:")
+        print(f"\nIntervalo que começa em {time_intervals[i][0]}s e termina em {time_intervals[i][1]}s:")
         print(f"Posição: {pos_values[i]}")
         print(f"Velocidade: {Vd_t}")
         print(f"Aceleração: {Ad_t}")
@@ -25,10 +25,9 @@ def differentiate(value, t, v0, s0):
 def main():
     # Inserir aqui os valores de aceleração, tempo e posição
     acc_values = [0, 2, 0, -4, 0]
-    interval = [(0, 1), (1, 3), (3, 8), (8, 9), (9, 10)]
+    time_intervals = [(0, 1), (1, 3), (3, 8), (8, 9), (9, 10)]
     pos_values = [0, 0, 4, 8, 26]
 
-    find_functions(acc_values, interval, pos_values)
-
+    find_functions(acc_values, time_intervals, pos_values)
 
 main()
--- src/all_integrate.py
+++ src/all_integrate.py
@@ -1,24 +1,27 @@
 from sympy import symbols, integrate
 
-
-def find_functions(acc_values, time_values):
+def find_functions(acc_values, time_intervals):
     t, v0, s0 = symbols("t v0 s0")
 
     size = len(acc_values)
 
     for i in range(size):
         # Encontrar a função de velocidade e posição a partir da aceleração
-        Vi_t, Si_t = integrate_acc_to_pos(acc_values[i], t, v0, s0)
+        Vi_t, Si_t = integrate_acc_to_pos(acc_values[i], t, v0, s0, time_intervals[i])
 
-        print(f"\nIntervalo que começa em {time_values[i]}s:")
+        print(f"\nIntervalo que começa em {time_intervals[i][0]}s e termina em {time_intervals[i][1]}s:")
         print(f"a(t) = {acc_values[i]}")
         print(f"v(t) = {Vi_t}")
         print(f"s(t) = {Si_t}")
 
 
-def integrate_acc_to_pos(acceleration, t, v0, s0):
-    Vi_t = integrate(acceleration, t) + v0
-    Si_t = integrate(Vi_t, t) + s0
+def integrate_acc_to_pos(acceleration, t, v0, s0, time_interval):
+    # Calcular os valores iniciais de velocidade (v0) e posição (s0) para o intervalo de tempo
+    v0_val = integrate(acceleration, t).subs(t, time_interval[0]) + v0
+    s0_val = integrate(v0_val, t).subs(t, time_interval[0]) + s0
+    # Integrar para obter as funções de velocidade e posição
+    Vi_t = integrate(acceleration, t) + v0_val
+    Si_t = integrate(Vi_t, t) + s0_val
 
     return Vi_t, Si_t
 
@@ -26,10 +29,10 @@ def integrate_acc_to_pos(acceleration, t, v0, s0):
 def main():
     # Inserir aqui os valores de aceleração, tempo e posição
     acc_values = [0, 2, 0, -4, 0]
-    interval = [(0, 1), (1, 3), (3, 8), (8, 9), (9, 10)]
-    pos_values = [0, 0, 4, 8, 26]
+    time_intervals = [(0, 1), (1, 3), (3, 8), (8, 9), (9, 10)]
 
-    find_functions(time_values, pos_values)
+    find_functions(acc_values, time_intervals)
 
 
 main()
+pos_values = [0, 0, 4, 8, 26]
